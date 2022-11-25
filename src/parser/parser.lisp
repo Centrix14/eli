80e85355b97cea625b9c9418b7fdec1902829299
Centrix14 <artiom.zvyagin@yandex.ru>
2022-11-25 11:26:21 +0300

    tangled from eli.org

--- src/parser/parser.lisp
+++ src/parser/parser.lisp
@@ -1,7 +1,17 @@
+(defpackage :eli-parser
+  (:use :common-lisp)
+  (:export :parser
+		   :parser-buffer-word
+		   :parser-buffer-expression
+		   :parser-buffer-delimiter))
+
 (defclass parser ()
   ((buffer-word :reader parser-buffer-word
 				:initarg :buffer-word
-				:initform ""
+				:initform (make-array 0
+									  :fill-pointer 0
+									  :adjustable t
+									  :element-type 'character)
 				:type string
 				:documentation "The word buffer")
    (buffer-expression :reader parser-buffer-expression
@@ -16,5 +26,27 @@
 					 :initarg :buffer-delimiter
 					 :initform " "
 					 :type string
-					 :documentation "The buffer of delimiters")
-   (:documentation "The parser class")))
+					 :documentation "The buffer of delimiters"))
+  (:documentation "The parser class"))
+
+(defmethod pars-char ((parser-object parser) char)
+  (with-slots (buffer-word buffer-expression buffer-delimiter) parser-object
+	  (if (delimiterp parser-object char)
+		  (pars-char-handle-delimiter parser-object)
+		  (pars-char-handle-regular parser-object char)))
+  parser-object)
+
+(defun pars-char-handle-delimiter (parser-object)
+  (with-slots (buffer-word) parser-object
+	(format t "word: ~a~%" buffer-word)
+	(setf (fill-pointer buffer-word) 0)))
+
+(defun pars-char-handle-regular (parser-object char)
+  (with-slots (buffer-word) parser-object
+	(vector-push-extend char buffer-word)))
+
+(defun delimiterp (parser-object char)
+  (with-slots (buffer-delimiter) parser-object
+	(if (position char buffer-delimiter)
+		t
+		nil)))
