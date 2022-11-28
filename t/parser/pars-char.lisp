460558223a899f150a3ca56f406f7414d16707aa
Centrix14 <artiom.zvyagin@yandex.ru>
2022-11-28 19:20:50 +0300

    Tangled from eli.org

--- t/parser/pars-char.lisp
+++ t/parser/pars-char.lisp
@@ -4,3 +4,9 @@
 	  (test-string "abra cadabra babra "))
   (loop for char across test-string do
 	   (setf test-parser (pars-char test-parser char))))
+
+#||(let ((test-parser
+		(make-instance 'parser
+					   :buffer-delimiter " ")))
+  (format t "~a~%" (pars-char test-parser #\q)))
+|#
