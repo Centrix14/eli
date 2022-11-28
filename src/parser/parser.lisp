a2cd940bfef81d5c2568eb84fb7205ff37b192a1
Centrix14 <artiom.zvyagin@yandex.ru>
2022-11-28 19:20:18 +0300

    Tangled from eli.org

--- src/parser/parser.lisp
+++ src/parser/parser.lisp
@@ -26,24 +26,40 @@
 					 :initarg :buffer-delimiter
 					 :initform " "
 					 :type string
-					 :documentation "The buffer of delimiters"))
+					 :documentation "The buffer of delimiters")
+   (need-new-nesting-layer :reader parser-need-new-nesting-layer
+						   :initarg :need-new-nesting-layer
+						   :initform nil
+						   :type symbol
+						   :documentation
+						   "Indicates, that need to create new nesting layer"))
   (:documentation "The parser class"))
 
 (defmethod pars-char ((parser-object parser) char)
   (with-slots (buffer-word buffer-expression buffer-delimiter) parser-object
 	  (if (delimiterp parser-object char)
 		  (pars-char-handle-delimiter parser-object)
-		  (pars-char-handle-regular parser-object char)))
-  parser-object)
+		  (pars-char-handle-regular parser-object char))))
 
 (defun pars-char-handle-delimiter (parser-object)
-  (with-slots (buffer-word) parser-object
-	(format t "word: ~a~%" buffer-word)
-	(setf (fill-pointer buffer-word) 0)))
+  (format t "word: ~a~%" (slot-value parser-object 'buffer-word))
+  (make-instance 'parser
+				 :buffer-word (make-array 0
+										  :fill-pointer 0
+										  :adjustable t
+										  :element-type 'character)
+				 :buffer-expression (parser-buffer-expression parser-object)
+				 :buffer-delimiter (parser-buffer-delimiter parser-object)
+				 :need-new-nesting-layer (parser-need-new-nesting-layer parser-object)))
 
 (defun pars-char-handle-regular (parser-object char)
-  (with-slots (buffer-word) parser-object
-	(vector-push-extend char buffer-word)))
+  (make-instance 'parser
+				 :buffer-word (concatenate 'string
+										   (slot-value parser-object 'buffer-word)
+										   (string char))
+				 :buffer-expression (parser-buffer-expression parser-object)
+				 :buffer-delimiter (parser-buffer-delimiter parser-object)
+				 :need-new-nesting-layer (parser-need-new-nesting-layer parser-object)))
 
 (defun delimiterp (parser-object char)
   (with-slots (buffer-delimiter) parser-object
