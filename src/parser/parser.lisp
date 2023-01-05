73bb39ce08d28d2dea9062364bab387f9d079399
Centrix14 <artiom.zvyagin@yandex.ru>
2023-01-05 12:05:45 +0300

    New, small, purified from OOP LN parser.

--- src/parser/parser.lisp
+++ src/parser/parser.lisp
@@ -1,111 +1,40 @@
-(defpackage :eli-parser
-  (:use :common-lisp)
-  (:export :parser
-           :parser-buffer-word
-           :parser-buffer-expression
-           :parser-buffer-delimiter
+(defun read-next-word (stream &key (separators " "))
+  (if (end-of-stream-p stream)
+      nil
+      (%read-next-word% stream separators)))
 
-           :eval-nestings))
+(defun %read-next-word% (stream separators)
+  (let ((buffer (make-array 0
+                            :fill-pointer 0
+                            :adjustable t
+                            :element-type 'character))
+        (c
+          (read-char stream nil 'the-end)))
 
-(defclass parser ()
-  ;; buffers
-  ((buffer-word :reader parser-buffer-word
-                :initarg :buffer-word
-                :initform (make-array 0
-                                      :fill-pointer 0
-                                      :adjustable t
-                                      :element-type 'character)
-                :type string
-                :documentation "The word buffer")
-   (buffer-expression :reader parser-buffer-expression
-                      :initarg :buffer-expression
-                      :initform (make-array 0
-                                            :fill-pointer 0
-                                            :adjustable t
-                                            :element-type 'string)
-                      :type vector
-                      :documentation "The expression buffer")
-   (buffer-delimiter :reader parser-buffer-delimiter
-                     :initarg :buffer-delimiter
-                     :initform " "
-                     :type string
-                     :documentation "The buffer of delimiters")
+    (loop until (or
+                 (eql c 'the-end)
+                 (and (separatorp separators c)
+                      (> (length buffer) 0)))
+          do
+             (unless (separatorp separators c)
+                 (vector-push-extend c buffer))
 
-   ;; nesting
-   (nesting-degree :reader parser-nesting-degree
-                   :initarg :nesting-degree
-                   :initform 0
-                   :type integer
-                   :documentation "Indicates nesting degree of current word")
-   (nesting-booster :reader parser-nesting-booster
-                    :initarg :nesting-booster
-                    :initform "["
-                    :type string
-                    :documentation "Contains word, that increments nesting")
-   (nesting-reducer :reader parser-nesting-reducer
-                    :initarg :nesting-reducer
-                    :initform "]"
-                    :type string
-                    :documentation "Contains word, that decrements nesting"))
+             (setf c (read-char stream nil 'the-end)))
 
-  (:documentation "The parser class"))
+    buffer))
 
-(defmethod print-object ((parser-object parser) stream)
-  (format stream "~a ~a ~a ~a ~a ~a"
-          (parser-buffer-word parser-object)
-          (parser-buffer-expression parser-object)
-          (parser-buffer-delimiter parser-object)
-          (parser-nesting-degree parser-object)
-          (parser-nesting-booster parser-object)
-          (parser-nesting-reducer parser-object)))
+(defun separatorp (separators char)
+  (if (position char separators)
+      t
+      nil))
 
-(defmethod pars-char ((parser-object parser) char)
-  (with-slots (buffer-word buffer-expression buffer-delimiter) parser-object
-    (if (delimiterp parser-object char)
-        (pars-char-handle-delimiter parser-object)
-        (pars-char-handle-regular parser-object char))))
+(defun end-of-stream-p (stream)
+  (let ((c
+          (read-char stream nil 'the-end))
+        (result nil))
 
-(defun pars-char-handle-delimiter (parser-object)
-  (multiple-value-bind (current-nesting-degree updated-nesting-degree)
-      (eval-nestings parser-object)
-    (make-instance 'parser
-                   :buffer-word (make-array 0
-                                            :fill-pointer 0
-                                            :adjustable t
-                                            :element-type 'character)
-                   :buffer-expression (concatenate
-                                       'vector
-                                       (parser-buffer-expression parser-object)
-                                       (vector
-                                        (list current-nesting-degree
-                                              (parser-buffer-word parser-object))))
-                   :buffer-delimiter (parser-buffer-delimiter parser-object)
+    (if (eql c 'the-end)
+        (setf result t))
 
-                   :nesting-degree updated-nesting-degree
-                   :nesting-booster (parser-nesting-booster parser-object)
-                   :nesting-reducer (parser-nesting-reducer parser-object))))
-
-(defun pars-char-handle-regular (parser-object char)
-  (make-instance 'parser
-                 :buffer-word (concatenate 'string
-                                           (slot-value parser-object 'buffer-word)
-                                           (string char))
-                 :buffer-expression (parser-buffer-expression parser-object)
-                 :buffer-delimiter (parser-buffer-delimiter parser-object)
-
-                 :nesting-degree (parser-nesting-degree parser-object)
-                 :nesting-booster (parser-nesting-booster parser-object)
-                 :nesting-reducer (parser-nesting-reducer parser-object)))
-
-(defmethod pars-stream ((parser-object parser) stream)
-  (loop with c = (read-char stream nil 'the-end)
-        while (not (eql c 'the-end)) do
-          (setf parser-object (pars-char parser-object c))
-          (setf c (read-char stream nil 'the-end)))
-  (pars-char parser-object #\Space))
-
-(defun delimiterp (parser-object char)
-  (with-slots (buffer-delimiter) parser-object
-    (if (position char buffer-delimiter)
-        t
-        nil)))
+    (unread-char c stream)
+    result))
