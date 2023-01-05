111c40451dff3a6ec4dab706d612139f97d9e9ce
Centrix14 <artiom.zvyagin@yandex.ru>
2023-01-05 12:04:16 +0300

    remove test for pars-char

--- t/parser/pars-char.lisp
+++
@@ -1,7 +0,0 @@
-(let ((test-parser
-        (make-instance 'parser
-                       :buffer-delimiter (vector #\Space #\Tab)))
-      (test-string "abra [ cadabra [ var danbra [ zabra ] ] babra ] "))
-  (loop for char across test-string do
-    (setf test-parser (pars-char test-parser char)))
-  (format t "~a~%" test-parser))
