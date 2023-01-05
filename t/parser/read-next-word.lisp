e803f571dbe04dce8cfcac6cae6559a42a861d2a
Centrix14 <artiom.zvyagin@yandex.ru>
2023-01-05 12:11:38 +0300

    test for read-next-word

---
+++ t/parser/read-next-word.lisp
@@ -0,0 +1,16 @@
+(let ((str
+        "atom while
+
+rule [ while .seq:cond .seq:body ]
+     [ rule [ while 1 .seq ]
+            [ while cond body ] ]")
+      (seps (vector #\Space
+                    #\Newline
+                    #\Tab)))
+  (with-input-from-string (stream str)
+    (loop with word = (read-next-word stream
+                                      :separators seps)
+          until (null word)
+          do
+             (format t "~a~%" word)
+             (setf word (read-next-word stream :separators seps)))))
