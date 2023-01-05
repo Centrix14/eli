d10f4f6c0dc75395641aad15251a767f66d38eb9
Centrix14 <artiom.zvyagin@yandex.ru>
2023-01-05 11:58:54 +0300

    remove print-object example

--- examples/print-object.lisp
+++
@@ -1,5 +0,0 @@
-(let ((test-parser
-        (make-instance 'parser
-                       :buffer-word "This is a word."
-                       :buffer-delimiter ",. ")))
-  (format t "~%~%print-object example~%~a~%" test-parser))
