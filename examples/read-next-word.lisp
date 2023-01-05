0d8755e78a23e3f2afe4507fdef579d682ef5f4f
Centrix14 <artiom.zvyagin@yandex.ru>
2023-01-05 12:10:35 +0300

    read-next-word function usage example

---
+++ examples/read-next-word.lisp
@@ -0,0 +1,9 @@
+(let ((str "word1 word2")
+      (seps (vector #\Space
+                     #\Newline
+                     #\Tab)))
+  (with-input-from-string (stream str)
+    (loop with word = (read-next-word stream :separators seps)
+          until (null word) do
+            (format t "~a~%" word)
+            (setf word (read-next-word stream :separators seps)))))
