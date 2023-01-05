bb55a1527c6fff48c868f90c19c5b11a28886890
Centrix14 <artiom.zvyagin@yandex.ru>
2023-01-05 11:55:11 +0300

    remove pars-stream example

--- examples/pars-stream.lisp
+++
@@ -1,5 +0,0 @@
-(let ((test-parser
-        (make-instance 'parser)))
-  (with-input-from-string (stream "my LN expression")
-    (format t "~%~%pars-stream example~%~a~%"
-            (pars-stream test-parser stream))))
