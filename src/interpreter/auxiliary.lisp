6ca75156c03766947d3dcbcc8b0ea8d9c32f657b
Centrix14 <artiom.zvyagin@yandex.ru>
2023-01-07 12:30:49 +0300

    Auxiliary functions for LN interpreter, They make sources pure, and readable better.
    P.S. I can't stop this, help me!
    (Shakelisp C., «Lispello», page NaN).

---
+++ src/interpreter/auxiliary.lisp
@@ -0,0 +1,28 @@
+(defun get-nesting (word current-nesting)
+  (cond
+    ((string= word "[")
+     (values (1+ current-nesting) current-nesting))
+
+    ((string= word "]")
+     (values (1- current-nesting) (1- current-nesting)))
+
+    (t
+     (values current-nesting current-nesting))))
+
+(defun make-it-element (word nesting-degree)
+  (list nesting-degree word))
+
+(defun print-it (tree)
+  (loop for element across tree do
+    (format t "~a~a~%"
+            (print-it-get-nesting-string (first element))
+            (second element))))
+
+(defun print-it-get-nesting-string (nesting-degree)
+  (loop with result = (make-array 0
+                                  :fill-pointer 0
+                                  :adjustable t
+                                  :element-type 'character)
+        repeat nesting-degree do
+          (vector-push-extend #\Tab result)
+        finally (return-from print-it-get-nesting-string result)))
