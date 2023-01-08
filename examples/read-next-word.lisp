9b3c8d5736a74c064d2f773b71c46f65d8c3ea2f
Centrix14 <artiom.zvyagin@yandex.ru>
2023-01-08 13:26:08 +0300

    Examples directory structure changing: since here appeared examples of LN code, usage examples of eli functions separate into corresponding directory.

--- examples/read-next-word.lisp
+++
@@ -1,9 +0,0 @@
-(let ((str "word1 word2")
-      (seps (vector #\Space
-                     #\Newline
-                     #\Tab)))
-  (with-input-from-string (stream str)
-    (loop with word = (read-next-word stream seps)
-          until (null word) do
-            (format t "~a~%" word)
-            (setf word (read-next-word stream seps)))))
